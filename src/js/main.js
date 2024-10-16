be5f1d6f5bfeb2156c73981b0f2052d18ceb14ba
weterynarzfred <weterynarzfred@gmail.com>
2024-10-16 13:44:43 +0200

    model container

--- src/js/main.js
+++ src/js/main.js
@@ -4,26 +4,27 @@ import { LineSegments2, LineSegmentsGeometry, OrbitControls } from "three/exampl
 import { InteractionManager } from 'three.interactive';
 import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 
-const SCALE = 0.1;
+const SCALE = 1;
+const VIEW_ASPECT = 4 / 3;
+const container = document.getElementById('model');
+const bgColor = 0x111111;
 
 function initCamera({ renderer }) {
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
-  camera.position.y = 0.2;
+  const camera = new THREE.PerspectiveCamera(60, VIEW_ASPECT, 0.01, 1000);
   const controls = new OrbitControls(camera, renderer.domElement);
 
+  controls.enableZoom = false;
   controls.enableDamping = true;
-  controls.dampingFactor = 0.05;
-  controls.screenSpacePanning = false;
-  controls.minDistance = 0.2;
-  controls.maxDistance = 0.2;
+  controls.dampingFactor = 0.04;
+  controls.minDistance = 2;
+  controls.maxDistance = 2;
   controls.minPolarAngle = Math.PI * 0.4;
   controls.maxPolarAngle = Math.PI * 0.4;
 
   window.addEventListener('resize', onWindowResize);
   function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(container.clientWidth, container.clientWidth / VIEW_ASPECT);
   }
 
   return { camera, controls };
@@ -33,8 +34,9 @@ async function initScene() {
   const scene = new THREE.Scene();
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  renderer.setClearColor(bgColor);
+  renderer.setSize(container.clientWidth, container.clientWidth / VIEW_ASPECT);
+  document.getElementById('model').appendChild(renderer.domElement);
   const { camera, controls } = initCamera({ renderer });
 
   const interactionManager = new InteractionManager(
@@ -62,7 +64,7 @@ function loadModel() {
 async function displayModel({ scene, interactionManager }) {
   const model = await loadModel();
 
-  const meshMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide, polygonOffset: true, polygonOffsetFactor: 1, polygonOffsetUnits: 1 });
+  const meshMaterial = new THREE.MeshBasicMaterial({ color: bgColor, side: THREE.DoubleSide, polygonOffset: true, polygonOffsetFactor: 1, polygonOffsetUnits: 1 });
   const lineMaterial = new LineMaterial({
     color: 0xffffff,
     linewidth: 2,
@@ -72,7 +74,7 @@ async function displayModel({ scene, interactionManager }) {
 
   for (let i = 0; i < model.scene.children.length; i++) {
     const geometry = model.scene.children[i].geometry;
-    const edges = new THREE.EdgesGeometry(geometry, 30);
+    const edges = new THREE.EdgesGeometry(geometry, 23);
 
     const mesh = new THREE.Mesh(geometry, meshMaterial);
     mesh.scale.set(SCALE, SCALE, SCALE);
@@ -83,8 +85,7 @@ async function displayModel({ scene, interactionManager }) {
     mesh.addEventListener('mouseover', event => event.stopPropagation());
     mesh.addEventListener('mouseout', event => event.stopPropagation());
 
-    const lineSegmentsGeometry = new LineSegmentsGeometry();
-    lineSegmentsGeometry.fromEdgesGeometry(edges);
+    const lineSegmentsGeometry = new LineSegmentsGeometry().fromEdgesGeometry(edges);
     const lineSegments2 = new LineSegments2(lineSegmentsGeometry, lineMaterial);
     lineSegments2.scale.set(SCALE, SCALE, SCALE);
     lineSegments2.position.y -= 0.4 * SCALE;
@@ -92,28 +93,26 @@ async function displayModel({ scene, interactionManager }) {
   }
 
   return;
-
-
 }
 
-// function displayDots({ scene, interactionManager }) {
-//   const sphereGeometry = new THREE.IcosahedronGeometry(0.02, 1);
-//   const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-//   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-//   sphere.position.x = -1 * SCALE;
-//   sphere.position.y = (0.96667 - 0.4) * SCALE;
-//   sphere.position.z = -0.11667 * SCALE;
-//   scene.add(sphere);
+function displayDots({ scene, interactionManager }) {
+  const sphereGeometry = new THREE.IcosahedronGeometry(0.05 * SCALE, 2);
+  const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.x = -1 * SCALE;
+  sphere.position.y = (0.96667 - 0.4) * SCALE;
+  sphere.position.z = -0.11667 * SCALE;
+  scene.add(sphere);
 
-//   interactionManager.add(sphere);
-//   sphere.addEventListener('mouseover', () => {
-//     sphereMaterial.color.set(0xff0000);
-//   });
-//   sphere.addEventListener('mouseout', () => {
-//     sphereMaterial.color.set(0xffff00);
-//   });
-// }
+  interactionManager.add(sphere);
+  sphere.addEventListener('mouseover', () => {
+    sphereMaterial.color.set(0xff0000);
+  });
+  sphere.addEventListener('mouseout', () => {
+    sphereMaterial.color.set(0xffff00);
+  });
+}
 
 const { scene, camera, renderer, controls, interactionManager } = await initScene();
 await displayModel({ scene, interactionManager });
-// displayDots({ scene, interactionManager });
+displayDots({ scene, interactionManager });
